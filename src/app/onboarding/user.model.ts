fefd71697ca90e775e7884103b9ed84a5d2718af
bibhu <System4@192.168.0.3>
2019-12-07 14:48:05 +0530

    User Model updated

--- src/app/onboarding/user.model.ts
+++ src/app/onboarding/user.model.ts
@@ -5,6 +5,23 @@ export interface Address {
 	address: string;
 }
 
+export interface Company{
+	id: string;
+	name: string;
+	registredOn?: Date;
+	address?: Address
+}
+
+export interface ProjectDetails{
+	name: string;
+	startDate: Date;
+	endDate: Date;
+	technologies: string[];
+	description: string;
+	role: string;
+	company: Company;
+}
+
 export interface PersonalDetails {
 	firstName: string;
 	lastName: string;
@@ -23,10 +40,25 @@ export interface EducationalDetails {
 	highSchoolPassingYear: number;
 }
 
+export interface CompanyExperience {
+	joiningDate: Date;
+	LeavingDate?: Date;
+	company: Company;
+}
+
+export interface ProfessionalDetails{
+	totalExperience: number;
+	technologies: string[];
+	companyExperiences: CompanyExperience[];
+	projects: ProjectDetails[];
+}
+
 export interface User {
 	personalDetails: PersonalDetails;
 	educationalDetails: EducationalDetails;
+	professionalDetails: ProfessionalDetails;
 	permanentAddress?: Address;
 	presentAddress?: Address;
 	aboutMe: string;
+	isProfileCompleted: boolean
 }
